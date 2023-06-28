c3f700a8263b3c67f9b80da8c4f4270879c37fb6
everpcpc <git@everpcpc.com>
2023-06-28 12:10:58 +0800

    z

--- bindings/nodejs/src/lib.rs
+++ bindings/nodejs/src/lib.rs
@@ -11,3 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#[macro_use]
+extern crate napi_derive;
+
+use napi::bindgen_prelude::*;
