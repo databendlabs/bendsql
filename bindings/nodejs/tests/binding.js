ac0d6b1070db059a7a8dfb21d2072f724c5937ca
everpcpc <git@everpcpc.com>
2023-07-11 19:30:47 +0800

    z

---
+++ bindings/nodejs/tests/binding.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2021 Datafuse Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Client } from "../index.js";
+import { Given, When, Then } from "@cucumber/cucumber";
+
+const dsn = "databend://root:@localhost:8000/default?sslmode=disable";
+
+
+Given("A new Databend Driver Client", function () {
+    this.client = new Client(dsn);
+});
