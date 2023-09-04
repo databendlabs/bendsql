c3712d5dd6647b4374393a479b3ed4e765b528b0
everpcpc <git@everpcpc.com>
2023-09-05 07:49:33 +0800

    z

--- cli/tests/02-put.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "CREATE STAGE ss_temp" | ${BENDSQL}
-
-echo "ABCD" > /tmp/a1.txt
-echo "ABCD" > /tmp/a2.txt
-
-echo 'put fs:///tmp/a*.txt @ss_temp/abc' | ${BENDSQL}
-echo 'get @ss_temp/abc fs:///tmp/edf' | ${BENDSQL}
