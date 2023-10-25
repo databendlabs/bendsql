55327305a9098f9a7446a501404312995d3b796a
everpcpc <git@everpcpc.com>
2023-10-25 08:52:42 +0800

    fix: tokenize stage path

--- cli/tests/01-put.sh
+++ cli/tests/01-put.sh
@@ -3,8 +3,23 @@
 echo "DROP STAGE IF EXISTS ss_temp" | ${BENDSQL}
 echo "CREATE STAGE ss_temp" | ${BENDSQL}
 
-echo "ABCD" >/tmp/a1.txt
-echo "ABCD" >/tmp/a2.txt
+cat <<SQL | ${BENDSQL}
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+(
+    title VARCHAR,
+    author VARCHAR,
+    date VARCHAR
+);
+SQL
 
-echo 'put fs:///tmp/a*.txt @ss_temp/abc' | ${BENDSQL}
+echo 'put fs://cli/tests/data/b*.parquet @ss_temp/abc/' | ${BENDSQL}
 echo 'get @ss_temp/abc fs:///tmp/edf' | ${BENDSQL}
+
+cat <<SQL | ${BENDSQL}
+COPY INTO books FROM @ss_tmp/abc/ files=('books.parquet') FILE_FORMAT = (TYPE = PARQUET);
+SQL
+
+cat <<SQL | ${BENDSQL}
+SELECT * FROM books;
+SQL
