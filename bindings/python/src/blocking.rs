22064fd64fb295bf1b2346f239b451968698bb8e
hantmac <hantmac@outlook.com>
2023-12-25 17:53:25 +0800

    ci

--- bindings/python/src/blocking.rs
+++ bindings/python/src/blocking.rs
@@ -86,14 +86,16 @@ impl BlockingDatabendConnection {
 
     pub fn query_iter(&self, sql: String) -> PyResult<RowIterator> {
         let this = self.0.clone();
-        let rt = tokio::runtime::Runtime::new()
-            .map_err(|e| PyErr::new::<exc::Exception, _>(format!("Error creating Tokio runtime: {:?}", e)))?;
+        let rt = tokio::runtime::Runtime::new().map_err(|e| {
+            PyErr::new::<exc::Exception, _>(format!("Error creating Tokio runtime: {:?}", e))
+        })?;
         // Use the runtime to block on the synchronous operation
         let ret = rt.block_on(async move {
-                        let streamer = this.query_iter(&sql)
-                            .map_err(|e| PyErr::new::<exc::Exception, _>(format!("Error querying: {:?}", e)))?;
-                     streamer
-                       });
+            let streamer = this
+                .query_iter(&sql)
+                .map_err(|e| PyErr::new::<exc::Exception, _>(format!("Error querying: {:?}", e)))?;
+            streamer
+        });
 
         Ok(RowIterator::new(ret))
     }
