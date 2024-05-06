db7d049f9c72dc0e504042de6a5cf95d6847d449
sundy-li <543950155@qq.com>
2024-05-06 08:54:20 +0800

    fix: fix args parser

--- cli/test.sh
+++ cli/test.sh
@@ -35,7 +35,7 @@ case $TEST_HANDLER in
 	echo "==> Testing REST API handler"
 	export BENDSQL="${CARGO_TARGET_DIR}/debug/bendsql"
 	
-	echo "create user if not exists databend identified by 'databend'" |  bendsql -dsn="databend+http://${DATABEND_USER}:${DATABEND_PASSWORD}@${DATABEND_HOST}:8000/?sslmode=disable&presign=on"
+	echo "create user if not exists databend identified by 'databend'" |  $BENDSQL -dsn="databend+http://${DATABEND_USER}:${DATABEND_PASSWORD}@${DATABEND_HOST}:8000/?sslmode=disable&presign=on"
 	export BENDSQL_NEW="${BENDSQL} --user databend --password databend --host ${DATABEND_HOST} --port 8000"
 	$BENDSQL_NEW --query="select 1 from numbers(10) where number > 1000"
 	
