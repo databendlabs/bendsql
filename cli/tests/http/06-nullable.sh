5511f3535a00c70911988e19b0c07fab2e55a2c2
everpcpc <git@everpcpc.com>
2024-07-31 15:06:11 +0800

    z

--- cli/tests/http/06-nullable.sh
+++ cli/tests/http/06-nullable.sh
@@ -4,14 +4,29 @@ DATABEND_USER=${DATABEND_USER:-root}
 DATABEND_PASSWORD=${DATABEND_PASSWORD:-}
 DATABEND_HOST=${DATABEND_HOST:-localhost}
 
+cat <<SQL | ${BENDSQL}
+DROP TABLE IF EXISTS books_06;
+CREATE TABLE books_06
+(
+    title VARCHAR,
+    author VARCHAR,
+    date VARCHAR,
+    comment VARCHAR
+);
+SQL
+
+cat <<SQL | ${BENDSQL}
+INSERT INTO books_06 (title, author, date) VALUES ('foo', 'bar', '2021-01-01');
+SQL
+
 export BENDSQL_DSN="databend+http://${DATABEND_USER}:${DATABEND_PASSWORD}@${DATABEND_HOST}:8000/?sslmode=disable&presign=on&format_null_as_str=0"
 
 cat <<SQL | ${BENDSQL} --output=csv
-select NULL, 1+1, '1+1', 'NULL';
+select * from books_06;
 SQL
 
 export BENDSQL_DSN="databend+http://${DATABEND_USER}:${DATABEND_PASSWORD}@${DATABEND_HOST}:8000/?sslmode=disable&presign=on&format_null_as_str=1"
 
 cat <<SQL | ${BENDSQL} --output=csv
-select NULL, 1+1, '1+1', 'NULL';
+select * from books_06;
 SQL
