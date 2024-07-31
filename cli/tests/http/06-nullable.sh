faf26d2c2eddc6df3918fa7939a7dda982f98fa6
everpcpc <git@everpcpc.com>
2024-07-31 14:46:21 +0800

    z

--- cli/tests/http/06-nullable.sh
+++ cli/tests/http/06-nullable.sh
@@ -7,19 +7,11 @@ DATABEND_HOST=${DATABEND_HOST:-localhost}
 export BENDSQL_DSN="databend+http://${DATABEND_USER}:${DATABEND_PASSWORD}@${DATABEND_HOST}:8000/?sslmode=disable&presign=on&format_null_as_str=0"
 
 cat <<SQL | ${BENDSQL} --quote-style=always
-select NULL;
-SQL
-
-cat <<SQL | ${BENDSQL} --quote-style=always
-select 'NULL';
+select NULL, 1, 'NULL';
 SQL
 
 export BENDSQL_DSN="databend+http://${DATABEND_USER}:${DATABEND_PASSWORD}@${DATABEND_HOST}:8000/?sslmode=disable&presign=on&format_null_as_str=1"
 
 cat <<SQL | ${BENDSQL} --quote-style=always
-select NULL;
-SQL
-
-cat <<SQL | ${BENDSQL} --quote-style=always
-select 'NULL';
+select NULL, 1, 'NULL';
 SQL
