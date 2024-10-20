c5fa4f527277f3dc6b3c9d971856fdd1735b957b
Maricaya <915270549@qq.com>
2024-10-20 21:02:08 +0000

    feat: frontend ci

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   // darkMode: false, // or 'media' or 'class'
-  content: [],
   theme: {
     extend: {},
   },
