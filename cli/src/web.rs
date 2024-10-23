8960f38c70eba7c82488ccbe4b135a881e18fc39
Maricaya <915270549@qq.com>
2024-10-23 03:19:01 +0000

    fix: add a blank line after license

--- cli/src/web.rs
+++ cli/src/web.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use actix_files as fs;
 use actix_web::middleware::Logger;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
