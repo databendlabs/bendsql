4c981366c9537b2cd5b3daa7a77cf72ed0ade38d
everpcpc <git@everpcpc.com>
2025-01-20 16:00:28 +0800

    z

--- bindings/python/tests/cursor/steps/binding.py
+++ bindings/python/tests/cursor/steps/binding.py
@@ -60,39 +60,42 @@ async def _(context):
     # Binary
     context.cursor.execute("select to_binary('xyz')")
     row = context.cursor.fetchone()
-    assert row[0] == b"xyz", f"Binary: {row.values()}"
+    expected = (b"xyz",)
+    assert row.values() == expected, f"Binary: {row.values()}"
 
     # Interval
     context.cursor.execute("select to_interval('1 days')")
     row = context.cursor.fetchone()
-    assert row.values() == (timedelta(1),), f"Interval: {row.values()}"
+    expected = (timedelta(1),)
+    assert row.values() == expected, f"Interval: {row.values()}"
 
     # Decimal
     context.cursor.execute("SELECT 15.7563::Decimal(8,4), 2.0+3.0")
     row = context.cursor.fetchone()
-    assert row.values() == (
-        Decimal("15.7563"),
-        Decimal("5.0"),
-    ), f"Decimal: {row.values()}"
+    expected = (Decimal("15.7563"), Decimal("5.0"))
+    assert row.values() == expected, f"Decimal: {row.values()}"
 
     # Array
     context.cursor.execute("select [10::Decimal(15,2), 1.1+2.3]")
     row = context.cursor.fetchone()
-    assert row.values() == ([Decimal("10.00"), Decimal("3.40")],), (
-        f"Array: {row.values()}"
-    )
+    expected = [Decimal("10.00"), Decimal("3.40")]
+    assert row.values() == expected, f"Array: {row.values()}"
 
     # Map
     context.cursor.execute("select {'xx':to_date('2020-01-01')}")
     row = context.cursor.fetchone()
-    assert row.values() == ({"xx": date(2020, 1, 1)},), f"Map: {row.values()}"
+    expected = {"xx": date(2020, 1, 1)}
+    assert row.values() == expected, f"Map: {row.values()}"
 
     # Tuple
     context.cursor.execute("select (10, '20', to_datetime('2024-04-16 12:34:56.789'))")
     row = context.cursor.fetchone()
-    assert row.values() == ((10, "20", datetime(2024, 4, 16, 12, 34, 56, 789000)),), (
-        f"Tuple: {row.values()}"
+    expected = (
+        10,
+        "20",
+        datetime(2024, 4, 16, 12, 34, 56, 789000),
     )
+    assert row.values() == expected, f"Tuple: {row.values()}"
 
 
 @then("Select numbers should iterate all rows")
