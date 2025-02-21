2141871d07b99924f209bed76ef9c3eaef17cfbe
sundyli <543950155@qq.com>
2025-02-21 10:49:16 +0800

    chore: enable duckdb crate only in x86-64

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -155,7 +155,7 @@ impl Settings {
         self.expand = cfg
             .expand
             .map(|expand| expand.as_str().into())
-            .unwrap_or_else(|| self.expand.clone());
+            .unwrap_or_else(|| self.expand);
         self.replace_newline = cfg.replace_newline.unwrap_or(self.replace_newline);
         self.max_width = cfg.max_width.unwrap_or(self.max_width);
         self.max_col_width = cfg.max_col_width.unwrap_or(self.max_col_width);
