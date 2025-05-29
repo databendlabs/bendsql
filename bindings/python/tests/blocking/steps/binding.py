754034f2608d22510e57b67f4bfda542a08a4ba0
everpcpc <git@everpcpc.com>
2025-05-29 12:22:27 +0800

    z

--- bindings/python/tests/blocking/steps/binding.py
+++ bindings/python/tests/blocking/steps/binding.py
@@ -152,7 +152,7 @@ def _(context):
     ]
     progress = context.conn.stream_load("INSERT INTO test VALUES", values)
     assert progress.write_rows == 3, f"progress.write_rows: {progress.write_rows}"
-    assert progress.write_bytes == 193, f"progress.write_bytes: {progress.write_bytes}"
+    assert progress.write_bytes == 194, f"progress.write_bytes: {progress.write_bytes}"
 
     rows = context.conn.query_iter("SELECT * FROM test")
     ret = []
@@ -172,7 +172,7 @@ def _(context):
         "INSERT INTO test VALUES", "tests/data/test.csv", {"type": "CSV"}
     )
     assert progress.write_rows == 3, f"progress.write_rows: {progress.write_rows}"
-    assert progress.write_bytes == 193, f"progress.write_bytes: {progress.write_bytes}"
+    assert progress.write_bytes == 194, f"progress.write_bytes: {progress.write_bytes}"
 
     rows = context.conn.query_iter("SELECT * FROM test")
     ret = []
