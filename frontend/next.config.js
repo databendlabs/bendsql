efc62617d10c0c6df0f507bde0e1b6cf6abe7693
sundyli <543950155@qq.com>
2025-10-18 23:11:40 +0800

    migrate to nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,55 @@ const nextConfig = {
   distDir: 'build',
   images: {
     unoptimized: true
+  },
+  // Generate fixed filenames to reduce git changes
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Use fixed filenames instead of content hashes
+      config.output.filename = 'static/js/[name].js'
+      config.output.chunkFilename = 'static/js/[name].js'
+      
+      // Ensure CSS filenames are also fixed
+      const miniCssExtractPlugin = config.plugins.find(
+        plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+      )
+      if (miniCssExtractPlugin) {
+        miniCssExtractPlugin.options.filename = 'static/css/[name].css'
+        miniCssExtractPlugin.options.chunkFilename = 'static/css/[name].css'
+      }
+
+      // Disable dynamic chunk naming in code splitting
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          ...config.optimization.splitChunks,
+          cacheGroups: {
+            ...config.optimization.splitChunks.cacheGroups,
+            default: false,
+            vendors: false,
+            // Merge all vendor code into one file
+            vendor: {
+              name: 'vendor',
+              chunks: 'all',
+              test: /node_modules/,
+              filename: 'static/js/vendor.js'
+            },
+            // Merge all shared code
+            common: {
+              name: 'common',
+              chunks: 'all',
+              minChunks: 2,
+              filename: 'static/js/common.js'
+            }
+          }
+        }
+      }
+    }
+    return config
+  },
+  // Ensure build ID is stable
+  generateBuildId: async () => {
+    return 'stable-build-id'
   }
 }
 
