9d6768de9093f7c1688e8d0a57c678754f7b230b
kould <kould2333@gmail.com>
2025-11-06 09:03:20 +0800

    fix: DataType::TimestampTz display: "TimestampTz" -> "Timestamp_Tz"

--- sql/src/schema.rs
+++ sql/src/schema.rs
@@ -138,7 +138,7 @@ impl std::fmt::Display for DataType {
                 write!(f, "Decimal({}, {})", size.precision, size.scale)
             }
             DataType::Timestamp => write!(f, "Timestamp"),
-            DataType::TimestampTz => write!(f, "TimestampTz"),
+            DataType::TimestampTz => write!(f, "Timestamp_Tz"),
             DataType::Date => write!(f, "Date"),
             DataType::Nullable(inner) => write!(f, "Nullable({inner})"),
             DataType::Array(inner) => write!(f, "Array({inner})"),
