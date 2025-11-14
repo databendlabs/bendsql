247a3677ede802f128eecd18af13fad4b64997a2
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-11-14 20:12:27 +0800

    fix

--- bindings/python/tests/blocking/steps/binding.py
+++ bindings/python/tests/blocking/steps/binding.py
@@ -154,31 +154,34 @@ def _(context):
         context.conn.exec(f"set timezone='{tz}'")
         row = context.conn.query_row("select to_datetime('2024-04-16 12:34:56.789')")
         exp = datetime(2024, 4, 16, 12, 34, 56, 789000, tzinfo=tz_expected)
-        assert row.values()[0] == exp, f"Tuple: {row.values()}"
+        assert row.values()[0] == exp, f"datetime(session level tz): {row.values()}"
         context.conn.exec("set timezone='UTC'")
 
         if DB_VERSION >= (1, 2, 839):
-            exp = datetime(2024, 4, 16, 12, 34, 56, 789000, tzinfo=tz_expected)
             row = context.conn.query_row(
                 f"settings(timezone='{tz}') select to_datetime('2024-04-16 12:34:56.789')"
             )
-            assert row.values()[0] == exp, f"Tuple: {row.values()}"
+            assert row.values()[0] == exp, f"datetime(query level tz): {row.values()}"
 
             row = context.conn.query_row(
-                f"settings(timezone='{tz}') select (to_datetime('2024-04-16 12:34:56.789'), 10)"
+                f"settings(timezone='{tz}') select to_datetime('2024-04-16 12:34:56.789'), 10"
             )
-            assert row.values()[0] == exp, f"Tuple: {row.values()}"
+            assert row.values()[0] == exp, (
+                f"datetime in Tuple: {row.values()[0]} != {exp}"
+            )
 
         tz_expected = timezone(timedelta(hours=6))
         row = context.conn.query_row(
             f"settings(timezone='{tz}') select to_timestamp_tz('2024-04-16 12:34:56.789 +0600')"
         )
-        if DB_VERSION >= (1, 2, 840):
-            exp = datetime(2024, 4, 16, 12, 34, 56, 789000, tzinfo=tz_expected)
+        exp = datetime(2024, 4, 16, 12, 34, 56, 789000, tzinfo=tz_expected)
+        exp_bug = datetime(2024, 4, 16, 18, 34, 56, 789000, tzinfo=tz_expected)
+        if DB_VERSION >= (1, 2, 840) and os.getenv("BODY_FORMAT") == "json":
+            assert row.values()[0] == exp, f"timestamp_tz: {row.values()[0]} {exp}"
         else:
-            # bug
-            exp = datetime(2024, 4, 16, 18, 34, 56, 789000, tzinfo=tz_expected)
-        assert row.values()[0] == exp, f"Tuple: {row.values()[0]} {exp}"
+            assert row.values()[0] == exp_bug, (
+                f"timestamp_tz: {row.values()[0]} {exp_bug}"
+            )
 
 
 @then("Select numbers should iterate all rows")
